35e011bf4ca6e153bbf51ebc54ef3deed9862678
fasilmsageer <fasilmsageer@gmail.com>
2022-12-06 10:57:06 +0530

    first

---
+++ src/main/java/com/example/Coursedb_backend/CoursedbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Coursedb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursedbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursedbBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Coursedb_backend/controller/Course.java
@@ -0,0 +1,4 @@
+package com.example.Coursedb_backend.controller;
+
+public class Course {
+}
