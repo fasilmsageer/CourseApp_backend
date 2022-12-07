68d8894fbfd991de53b43ca3199ac99180487ee4
fasilmsageer <fasilmsageer@gmail.com>
2022-12-07 16:08:05 +0530

    searchcourse

--- src/main/java/com/example/Coursedb_backend/dao/CourseDao.java
+++ src/main/java/com/example/Coursedb_backend/dao/CourseDao.java
@@ -1,7 +1,14 @@
 package com.example.Coursedb_backend.dao;
 
 import com.example.Coursedb_backend.model.Course;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CourseDao extends CrudRepository<Course,Integer> {
+
+    @Query(value = "SELECT `id`, `course_title`, `coursedate`, `coursedescription`, `courseduration`, `coursevenue`, `coursetitle` FROM `course` WHERE `coursetitle`=:coursetitle",nativeQuery = true)
+    List<Course> SearchCourse(@Param("coursetitle") String coursetitle);
 }
